afd2c8700bd27eb9d6b591820357c4e215c5beec
Elahesmailpour <eli_es87@yahoo.com>
2021-06-10 15:35:32 +0200

    todolist-project

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+itemcounter:number=0;
+projecttext:string='Angular Project';
 
-  constructor() { }
+  projects: string[] = []; 
+
+  constructor() { 
+
+  }
 
   ngOnInit(): void {
+    this.itemcounter=this.projects.length;
+  }
+  additem(){
+this.projects.push(this.projecttext)
+this.projecttext= "";
+this.itemcounter=this.projects.length;
+  }
+  removeitem(i:any){
+    //this.projects.pop();
+    this.projects.splice(i, 1);
   }
 
 }
