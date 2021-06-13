eb722cce162475ac37082a4261bc3426353ffa13
Elahesmailpour <eli_es87@yahoo.com>
2021-06-13 22:52:30 +0200

    route-Navigation

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,6 @@
+
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-projects',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) {
 
+  }
+  NavHome() {
+    this.route.navigate([""])
+//this.route.navigate(["/profile"],{queryParams : {id : 1}})
+  }
   ngOnInit(): void {
+
   }
 
 }
